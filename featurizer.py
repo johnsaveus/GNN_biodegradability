4d97cb8c4a18dc41cbc654bea27cf4f65e282e72
johnsaveus <ioannis.savvas00@gmail.com>
2024-05-05 13:01:48 +0300

    featurizing_complete_needs_testing

--- featurizer.py
+++ featurizer.py
@@ -1,6 +1,8 @@
 import torch
 from rdkit import Chem
 import pandas as pd
+from torch.utils.data import Dataset
+from torch_geometric.data import Data
 
 def one_of_k_encoding(x, allowable_set):
     if x not in allowable_set:
@@ -71,16 +73,16 @@ def atom_features(atom):
                 Chem.rdchem.HybridizationType.SP3D, 
                 Chem.rdchem.HybridizationType.SP3D2,
                 'other'
-                ]) + one_of_k_encoding_unk(atom.GetTotalNumHs(),
+                ]) + one_of_k_encoding(atom.GetTotalNumHs(),
                                            [0, 1, 2, 3, 4]) + \
                     one_of_k_encoding(atom.GetDegree(), 
                                       [0, 1, 2, 3, 4]) + \
                     [atom.GetFormalCharge()] + \
                     [atom.GetIsAromatic]
     
-    return atom_feats
+    return torch.tensor(atom_feats)
+ 
 
-'''
 def bond_features(bond):
    
    bt = bond.GetBondType()
@@ -92,4 +94,39 @@ def bond_features(bond):
         bond.GetStereo()]
    
    return bond_feats
-'''
+
+
+def nodes_and_adjacency(smile, y):
+
+    mol = Chem.MolFromSmiles(smile)
+
+    node_features = [atom_features(atom) for atom in mol.GetAtoms()]
+    node_features = torch.stack(node_features).float()
+
+    bond_features = [bond_features(bond) for bond in mol.GetBonds()]
+    bond_features = torch.stack(bond_features).float()
+    # Create Adjacency Matrix
+    ix1 , ix2 = [] , []
+    for bond in mol.GetBonds():
+        start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+        ix1 += [start, end]
+        ix2 += [end, start]
+    adj_norm = torch.asarray([ix1, ix2], dtype = torch.int64) # Needs to be in COO Format
+
+    return Data(x = node_features,
+                edge_index = adj_norm,
+                edge_attr = bond_features,
+                y = y)
+
+
+class MolecularDataset(Dataset):
+
+    def __init__(self,
+                 smiles,
+                 Y):
+        
+        self.smiles = smiles
+        self.Y = Y
+
+    def split():
+        # Only for creation of validation and test
