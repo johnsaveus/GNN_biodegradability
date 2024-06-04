bf4678dfc1358588b482a1679f26a73859b848f6
johnsaveus <ioannis.savvas00@gmail.com>
2024-06-04 10:56:48 +0300

    trainer

--- trainer.py
+++ trainer.py
@@ -1,30 +1,43 @@
 from torch.nn import BCELoss, BCEWithLogitsLoss
 from torch.optim import Adam
-from models import GraphAttention
+from models import GraphAttention, FPNN
 import torch
 from torch_geometric.loader import DataLoader
 from sklearn.metrics import confusion_matrix
 from featurizer import MolecularDataset
-#from torch.utils.data import DataLoader
+from torch_geometric.nn.models import GAT
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import global_add_pool
 
 loss_fn  = BCEWithLogitsLoss()
 epochs = 50
 
-dataset = MolecularDataset(root = 'data')
-dataset.load('data\processed\data.pt')
-train_data = dataset[1000:1500]
-test_data  = dataset[5000:6000]
-#for data in train_data:
- #   print(data)
+dataset_train = MolecularDataset(root = 'data', path_to_ind='data/split_ix/train_ix.txt', save_name='train')
+dataset_valid = MolecularDataset(root = 'data', path_to_ind='data/split_ix/valid_ix.txt', save_name='valid')
+dataset_test = MolecularDataset(root = 'data', path_to_ind='data/split_ix/test_ix.txt', save_name='test')
+dataset_train.load('data/processed/train.pt')
+dataset_valid.load('data/processed/valid.pt')
+dataset_test.load('data/processed/test.pt')
 
-train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
-model = GraphAttention(60, 20, 4)
+print(dataset_train[0])
+train_batches = len(dataset_train) / 64
+valid_batches = len(dataset_valid) / 64
+
+train_loader = DataLoader(dataset=dataset_train, batch_size=64, shuffle=True)
+validation_loader = DataLoader(dataset=dataset_valid, batch_size=64, shuffle=False)
+
+model = GraphAttention(60, 30, 4)
 optimizer = Adam(params = model.parameters(), lr = 0.001)
 
 for epoch in range(epochs):
-    total_loss = 0
+    model.train()
+    print(epoch)
+    train_loss = 0
+    test_loss = 0
     for data in train_loader:
-        
         x, edge_index, batch = data.x, data.edge_index, data.batch
         torch.manual_seed(42)
         y_pred = model(x, edge_index, batch)
@@ -34,21 +47,19 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        total_loss+= loss
-    print(total_loss/32)
-model.eval()
-y_preds = []
-y_true = []
-for data in test_data:
-    #print(data)
-    x, edge_index, y, batch = data.x, data.edge_index, data.batch, data.batch
-    y_pred = model(x, edge_index, batch)
-    #print(y_pred.item())
-    y_preds.append(y_pred.item())
-    y_true.append(data.y.tolist())
-    #print(data['y'].tolist())
-#print(y_true)
-y_preds = [1 if pred>=0 else 0 for pred in y_preds]
-
-#print(y_preds)
-print(confusion_matrix(y_true, y_preds))
+        train_loss+=loss.item()
+    train_loss = train_loss / train_batches
+    print('TRAINING ----')
+    print(train_loss)
+    model.eval()
+    with torch.no_grad():
+        for data in validation_loader:
+            x, edge_index, batch = data.x, data.edge_index, data.batch
+            torch.manual_seed(42)
+            y_pred = model(x, edge_index, batch)
+            # data.y is int. Need to be casted to float
+            loss = loss_fn(y_pred, data.y.float())
+            test_loss+= loss.item()
+    test_loss = test_loss / valid_batches
+    print('VAL------')
+    print(test_loss)
