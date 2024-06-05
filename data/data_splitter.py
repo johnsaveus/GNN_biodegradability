d1670909e933a554f3873346d952e58a33a2d61b
johnsaveus <ioannis.savvas00@gmail.com>
2024-06-05 19:12:20 +0300

    mr

--- data/data_splitter.py
+++ data/data_splitter.py
@@ -11,25 +11,21 @@ class Data_Splitter():
                  path_to_csv,
                  train_ratio = 0.80,
                  val_ratio = 0.10):
-        
         self.path_to_sdf = path_to_sdf
         self.path_to_csv = path_to_csv
         self.train_ratio = train_ratio
         self.val_ratio = val_ratio 
         self.test_ratio = 1 - train_ratio - val_ratio
-
         assert (train_ratio + val_ratio + self.test_ratio) == 1, 'Fractions should add to 1'
         self._read_csv()
         self._read_sdf()
 
     def _read_csv(self):
-
         data_csv = pd.read_csv(self.path_to_csv)
         self.smiles_csv = data_csv['SMILES']
         self.y_csv = data_csv['ReadyBiodegradability']
 
     def _read_sdf(self):
-
         reader = Chem.SDMolSupplier(self.path_to_sdf)
         self.smiles_sdf = []  # Smiles from property
         self.y_sdf = [] # RB
@@ -48,21 +44,17 @@ class Data_Splitter():
         self.y_sdf = sdf_data['ReadyBiodegradability']
 
     def split(self):
-        
         self._random_split(self.smiles_csv,
                            self.y_csv,
                            'csv')
-        
         self._random_split(self.smiles_sdf,
                            self.y_sdf,
                            'sdf')
         
-
     def _random_split(self,
                      smiles,
                      y,
                      dataset_name):
-
         smiles_train, smiles_val_test, _ , y_val_test = train_test_split(smiles,
                                                                       y,
                                                                       test_size = self.val_ratio + self.test_ratio,
@@ -76,7 +68,6 @@ class Data_Splitter():
                                                         stratify=y_val_test)
         valid_ix = smiles_val.index
         test_ix = smiles_test.index
-
         self._save_ix(train_ix, valid_ix, test_ix, dataset_name)
         
     def _save_ix(self,
@@ -96,11 +87,9 @@ class Data_Splitter():
         with open(train_ix_path, 'w') as file:
             for ix in train_ix:
                 file.write(f"{ix}\n")
-
         with open(valid_ix_path, 'w') as file:
             for ix in valid_ix:
                 file.write(f"{ix}\n")
-        
         with open(test_ix_path, 'w') as file:
             for ix in test_ix:
                 file.write(f"{ix}\n")
@@ -108,5 +97,4 @@ class Data_Splitter():
 if __name__ == '__main__':
     splitter = Data_Splitter(path_to_csv = 'raw/ECHA_biodegradability_new_compounds.csv',
                          path_to_sdf = 'raw/AllPublicnew.sdf')
-
     splitter.split()
