86068a369177cc578e51a8dfb04bbb4dd0e2afcf
johnsaveus <ioannis.savvas00@gmail.com>
2024-06-06 13:10:06 +0300

    New folder_for_architecture

---
+++ Net/gat_layer.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GraphAttentionLayer(nn.Module):
+
+    def __init__(self,
+                 in_feats,
+                 out_feats,
+                 drop_prob=0.5,
+                 leaky_relu_slope=0.2,
+                 init_distrib = 'uniform'
+                 ):
+        
+        super(GraphAttentionLayer, self).__init__()
+        self.out_feats = out_feats
+        self.init_distrib = init_distrib
+        self.leakyrelu = nn.LeakyReLU(negative_slope=leaky_relu_slope)
+        self.dropout = nn.Dropout(p=drop_prob)
+        # Weight matrix for the linear projection of node features W*h. 
+        # This is for a single attention head
+        self.W = nn.Parameter(torch.empty(size = (in_feats,out_feats)))
+        # Attention weight vector.
+        self.a = nn.Parameter(torch.empty(size= (2 * out_feats, 1)))
+        self._params_init()
+
+    def _params_init(self):
+        g = 1.414
+        if self.init_distrib == 'uniform':
+            nn.init.xavier_uniform_(self.W, gain=g)
+            nn.init.xavier_uniform_(self.a, gain=g)
+        elif self.init_distrib == 'normal':
+            nn.init.xavier_normal_(self.W, gain=g)
+            nn.init.xavier_normal_(self.a, gain=g)
+        else:
+            raise ValueError(f"Invalid init_distrib value = {self.init_distrib} Select 'uniform' or 'normal'")
+        
+    def _calc_attention_scores(self, linear_proj):
+        # Broadcasting
+        Wh_i = torch.matmul(linear_proj, self.a[:self.out_feats, :])
+        Wh_j = torch.matmul(linear_proj, self.a[self.out_feats:, :])
+        # Broadcast operation instead of concat.
+        Wh = Wh_i + Wh_j.mT
+        e_i_j = self.leakyrelu(Wh)
+        return e_i_j
+    
+    def _convert_adj_to_dense(self, x, adj):
+
+        num_nodes = x.shape[0]
+        vals = torch.ones(adj.shape[1], dtype = torch.float32)
+        sparse_adj = torch.sparse_coo_tensor(adj, vals, (num_nodes, num_nodes))
+        dense_adj = sparse_adj.to_dense()
+        bool_dense = dense_adj > 0
+        return bool_dense
+    def forward(self, x, adj):
+
+        #if adj.shape[0]!= x.shape[0] and adj.shape[1] != x.shape[1]:
+        adj = self._convert_adj_to_dense(x, adj)
+        #num_nodes = node_features.shape[0]
+        #assert edge_index.shape[0] == 2, f'Adjacency Matrix needs to be in COO format'
+        # Create W*h Matrix with heads.
+        # (num_nodes , in_feats) * (in_feats, out_feats).  Shape = (Num_nodes, output_feats)
+        # Multiplication without broadcasting
+        #print(x.shape)
+        linear_proj = torch.mm(x, self.W)
+        # Dropout to projection
+        #print(linear_proj.shape)
+        linear_proj_drop = self.dropout(linear_proj)
+        # Calculate e before normalizing. 
+        e = self._calc_attention_scores(linear_proj_drop)
+        # Only the connected nodes contribute to attention
+        # Init zero_vec with low values so it can be near zero when using softmax
+        softmax_zero_vals = -9e15*torch.ones_like(e)
+        attention = torch.where(adj > 0 , e, softmax_zero_vals)
+        attention = F.softmax(attention, dim=1)
+        attention = self.dropout(attention)
+        #print(attention.shape)
+        # Final multiplication for the input of the next layer.
+        # Reminder: 1 head
+        h_out = torch.matmul(attention, linear_proj)
+
+        return h_out
---
+++ Net/gat_network.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from Net.gat_layer import GraphAttentionLayer
+from torch_geometric.nn.pool import global_add_pool
+from data.mol_to_graph import MolecularDataset
+
+torch.manual_seed(42)
+class GraphAttention(nn.Module):
+
+    def __init__(self,
+                 atom_feats,
+                 hidden_feats,
+                 num_heads,
+                 num_layers=3,
+                 activation = nn.ELU(),
+                 drop_prob=0.2):
+        super(GraphAttention, self).__init__() 
+        self.drop_prob = drop_prob
+        self.num_heads = num_heads
+        self.activation = activation
+        self.fc1 = nn.Linear(hidden_feats * num_heads, hidden_feats)
+        self.fc2 = nn.Linear(hidden_feats, 1)
+        self.attention_layers = nn.ModuleList([
+            GraphAttentionLayer(atom_feats if i == 0 else hidden_feats * num_heads, 
+                                hidden_feats, 
+                                drop_prob=drop_prob) 
+            for i in range(num_layers)
+        ])
+
+    def forward(self, x, adj, batch):
+        #print(x.shape)
+        x = F.dropout(x, self.drop_prob, training = self.training)
+        #print(x.shape)
+        for attention_layer in self.attention_layers:
+            x = torch.cat([attention_layer(x, adj) for _ in range(self.num_heads)], dim = 1)
+            x = self.activation(x)
+            x = F.dropout(x, self.drop_prob, training = self.training)
+            #print(x.shape)
+        x = global_add_pool(x, batch)
+        #print(x.shape)
+        x = self.fc1(x)
+        #print(x.shape)
+        x = self.fc2(x)
+        #print(x.shape)
+        x = x.squeeze(dim = -1)
+        return x
